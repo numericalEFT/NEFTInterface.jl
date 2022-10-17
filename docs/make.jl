83211687b7f9c672cd5d94020226e89d8305918a
Kun Chen <chenkun0228@gmail.com>
2022-10-16 23:20:49 -0400

    fix CI

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ DocMeta.setdocmeta!(NEFTInterface, :DocTestSetup, :(using NEFTInterface); recurs
 makedocs(;
     modules=[NEFTInterface],
     authors="Kun Chen, Tao Wang, Xiansheng Cai, PengCheng Hou, and Zhiyi Li",
-    repo="https://github.com/numericaleft/NEFTInterface.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/numericalEFT/NEFTInterface.jl/blob/{commit}{path}#{line}",
     sitename="NEFTInterface.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
